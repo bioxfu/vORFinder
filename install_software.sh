60f98455976f55671d3b66aeea24df19081534db
Fu Xing <bio.xfu@gmail.com>
2018-06-15 16:05:59 +0800

    update

--- install_software.sh
+++ install_software.sh
@@ -1,12 +1,13 @@
 # install BLAST
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-*+-x64-linux.tar.gz
+wget -c ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-*+-x64-linux.tar.gz
 tar -zxvf ncbi-blast-*+-x64-linux.tar.gz
 mv ncbi-blast*/bin/makeblastdb software
 mv ncbi-blast*/bin/blastp software/
 rm -r ncbi-blast-*
 
 # install ORFfinder
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/TOOLS/ORFfinder/linux-i64/ORFfinder.gz
+wget -c ftp://ftp.ncbi.nlm.nih.gov/genomes/TOOLS/ORFfinder/linux-i64/ORFfinder.gz
 gzip -d ORFfinder.gz
+chmod +x ORFfinder
 mv ORFfinder software/
 
